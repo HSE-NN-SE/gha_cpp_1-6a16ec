d21a38bac6a8adae6082af0cef2dbb72a7919d82
6a16ec <semaev2014@yandex.ru>
2021-04-09 15:10:46 +0300

    Removing the old workflow

--- include/Manager.h
+++ include/Manager.h
@@ -5,15 +5,36 @@
 #ifndef INCLUDE_MANAGER_H_
 #define INCLUDE_MANAGER_H_
 
+class Project {
+ public:
+    Project() :
+                id(this->nextId())
+                , budget(0)
+                {}
+    uint16_t getId() const;
+    void setId(uint16_t id);
+    uint64_t getBudget() const;
+    void setBudget(uint64_t budget);
+
+ private:
+    uint16_t id;
+    uint64_t budget;
+    uint16_t nextId() const;
+};
+
 class ProjectManager : public Employee, ProjectBudget {
  public:
     explicit ProjectManager(Position position = Position::ProjectManager) :
                                                     Employee(position)
+                                                    , projects(new Project[1])
                                                     {}
+    ~ProjectManager();
     virtual void calc();
     void printInfo();
     int calcBudgetPart(float part, int budget) {return 0;}
     int calcProAdditions() {return 0;}
+ private:
+    Project *projects;
 };
 
 class SeniorManager : public ProjectManager {
--- src/Manager.cpp
+++ src/Manager.cpp
@@ -2,6 +2,32 @@
 #include <iostream>
 #include "../include/Manager.h"
 
+Project::~ProjectManager() {
+    delete[] this->projects;
+}
+
+uint16_t Project::nextId() const {
+    static uint16_t new_id = 0;
+    new_id += 1;
+    return new_id;
+}
+
+uint16_t Project::getId() const {
+    return this->id;
+}
+
+void Project::setId(uint16_t id) {
+    this->id = id;
+}
+
+uint64_t Project::getBudget() const {
+    return this->budget;
+}
+
+void Project::setBudget(uint64_t budget) {
+    this->budget = budget;
+}
+
 void ProjectManager::calc() {}
 
 void ProjectManager::printInfo() {
